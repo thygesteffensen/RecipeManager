0099da15da36d3b258c0f2c6d57dfb1f673124a3
Thyge S. Steffensen <s175176@student.dtu.dk>
2019-03-19 10:38:34 +0100

    First commit

--- RecipeManager/MainWindow.xaml.cs
+++ RecipeManager/MainWindow.xaml.cs
@@ -12,17 +12,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RecipeManager;
 
-namespace RecipeManager
+namespace HelloWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindows controlMainWindows;
         public MainWindow()
         {
             InitializeComponent();
+            controlMainWindows = new MainWindows();
+        }
+
+        private void FillOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] strArr = controlMainWindows.getCategories();
+            foreach (string varStr in strArr)
+            {
+                TextBlock printTextBlock = new TextBlock();
+                printTextBlock.Text = varStr;
+                StackView.Children.Add(printTextBlock);
+            }
+        }
+
+        private void categoryClickHandler(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
