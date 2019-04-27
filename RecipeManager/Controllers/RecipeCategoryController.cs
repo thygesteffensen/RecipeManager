7afa66a297f3ff5caf3b46e3df5ca52e3c091868
Thyge S. Steffensen <s175176@student.dtu.dk>
2019-04-27 09:34:42 +0200

    Added recipe cat controller

---
+++ RecipeManager/Controllers/RecipeCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipeManager.Models;
+
+namespace RecipeManager.Controllers
+{
+    class RecipeCategoryController
+    {
+        private RecipeCategoryModel recipeCategoryModel;
+
+        public RecipeCategoryController(SqlConnection sqlConnection)
+        {
+            recipeCategoryModel = new RecipeCategoryModel(sqlConnection);
+        }
+
+        public List<RecipeCategory> GetRecipeCategories()
+        {
+            return recipeCategoryModel.GetRecipeCategories();
+        }
+
+        public void CreateRecipeCategory(string recipeCategoryName)
+        {
+            recipeCategoryModel.CreateRecipeCategory(recipeCategoryName);
+        }
+    }
+}
